50b4e5f7be5404b5703699fe8ed8ef0d13221050
fridtjof <2780577+fridtjof@users.noreply.github.com>
2018-11-26 10:17:14 +0100

    SCSIcdimage: names, misc improvements

--- ILO2VirtDevs/vtd191p09.jar.src/com/hp/ilo2/virtdevs/SCSIcdimage.java
+++ ILO2VirtDevs/vtd191p09.jar.src/com/hp/ilo2/virtdevs/SCSIcdimage.java
@@ -6,20 +6,16 @@ import java.io.InputStream;
 import java.net.Socket;
 
 public class SCSIcdimage extends SCSI {
-    int fdd_state = 0;
-    int event_state = 0;
-    long mediaSize;
-    virtdevs cdi;
+    private int fddState = 0;
+    private int eventState = 0;
+    private long mediaSize;
+    private virtdevs cdi;
 
     public SCSIcdimage(Socket paramSocket, InputStream paramInputStream, BufferedOutputStream paramBufferedOutputStream, String paramString, virtdevs paramvirtdevs) throws IOException {
         super(paramSocket, paramInputStream, paramBufferedOutputStream, paramString, 0);
         this.cdi = paramvirtdevs;
         int i = this.media.open(paramString, 0);
         D.println(D.INFORM, "Media open returns " + i + " / " + this.media.size() + " bytes");
-    }
-
-    public void setWriteProt(boolean writeProtect) {
-        this.writeprot = writeProtect;
     }
 
     public void process() throws IOException {
@@ -29,22 +25,22 @@ public class SCSIcdimage extends SCSI {
         D.hexdump(D.INFORM, this.req, 12);
 
         this.mediaSize = this.media.size();
-        if (this.mediaSize == 0L) {
+        if (this.mediaSize == 0) {
             this.reply.setmedia(0);
-            this.fdd_state = 0;
+            this.fddState = 0;
 
-            this.event_state = 4;
+            this.eventState = 4;
         } else {
             this.reply.setmedia(1);
-            this.fdd_state += 1;
-            if (this.fdd_state > 2) {
-                this.fdd_state = 2;
+            this.fddState += 1;
+            if (this.fddState > 2) {
+                this.fddState = 2;
             }
-            if (this.event_state == 4)
-                this.event_state = 0;
-            this.event_state += 1;
-            if (this.event_state > 2) {
-                this.event_state = 2;
+            if (this.eventState == 4)
+                this.eventState = 0;
+            this.eventState += 1;
+            if (this.eventState > 2) {
+                this.eventState = 2;
             }
         }
         switch (this.req[0] & 0xFF) {
@@ -84,43 +80,43 @@ public class SCSIcdimage extends SCSI {
         }
     }
 
-    void client_send_diagnostic() throws IOException {}
+    private void client_send_diagnostic() throws IOException {}
 
-    void client_read(byte[] request) throws IOException {
+    private void client_read(byte[] request) throws IOException {
         int j = request[0] == SCSI_READ_12 ? 1 : 0;
 
         long l = SCSI.mk_int32(request, 2) * 2048;
-        int i = j != 0 ? SCSI.mk_int32(request, 6) : SCSI.mk_int16(request, 7);
-        i *= 2048;
+        int len = j != 0 ? SCSI.mk_int32(request, 6) : SCSI.mk_int16(request, 7);
+        len *= 2048;
 
-        D.println(D.VERBOSE, "CDImage :Client read " + l + ", len=" + i);
+        D.println(D.VERBOSE, "CDImage :Client read " + l + ", len=" + len);
 
-        if (this.fdd_state == 0) {
+        if (this.fddState == 0) {
             D.println(D.VERBOSE, "media not present");
             this.reply.set(2, 58, 0, 0);
-            i = 0;
-        } else if (this.fdd_state == 1) {
+            len = 0;
+        } else if (this.fddState == 1) {
             D.println(D.VERBOSE, "media changed");
             this.reply.set(6, 40, 0, 0);
-            i = 0;
-            this.fdd_state = 2;
+            len = 0;
+            this.fddState = 2;
 
         } else if ((l >= 0) && (l < this.mediaSize)) {
-            this.media.read(l, i, this.buffer);
-            this.reply.set(0, 0, 0, i);
+            this.media.read(l, len, this.buffer);
+            this.reply.set(0, 0, 0, len);
         } else {
             this.reply.set(5, 33, 0, 0);
-            i = 0;
+            len = 0;
         }
 
 
         this.reply.send(this.out);
-        if (i != 0)
-            this.out.write(this.buffer, 0, i);
+        if (len != 0)
+            this.out.write(this.buffer, 0, len);
         this.out.flush();
     }
 
-    void client_pa_media_removal(byte[] request) throws IOException {
+    private void client_pa_media_removal(byte[] request) throws IOException {
         if ((request[4] & 0x1) != 0) {
             D.println(D.VERBOSE, "Media removal prevented");
         } else {
@@ -132,21 +128,21 @@ public class SCSIcdimage extends SCSI {
     }
 
 
-    void client_start_stop_unit(byte[] request) throws IOException {
+    private void client_start_stop_unit(byte[] request) throws IOException {
         int i = (byte) (request[4] & 0x3);
 
         if (i == 3) {
             if (this.cdi.cdConnection != null) {
-                this.fdd_state = 1;
-                this.event_state = 2;
+                this.fddState = 1;
+                this.eventState = 2;
             } else {
-                this.fdd_state = 0;
-                this.event_state = 4;
+                this.fddState = 0;
+                this.eventState = 4;
             }
         } else if (i == 2) {
-            this.fdd_state = 0;
+            this.fddState = 0;
 
-            this.event_state = 4;
+            this.eventState = 4;
             if (this.cdi.cdConnection != null) {
                 this.cdi.do_cdrom(this.cdi.cdSelected);
             }
@@ -158,14 +154,14 @@ public class SCSIcdimage extends SCSI {
         this.out.flush();
     }
 
-    void client_test_unit_ready() throws IOException {
-        if (this.fdd_state == 0) {
+    private void client_test_unit_ready() throws IOException {
+        if (this.fddState == 0) {
             D.println(D.VERBOSE, "media not present");
             this.reply.set(2, 58, 0, 0);
-        } else if (this.fdd_state == 1) {
+        } else if (this.fddState == 1) {
             D.println(D.VERBOSE, "media changed");
             this.reply.set(6, 40, 0, 0);
-            this.fdd_state = 2;
+            this.fddState = 2;
         } else {
             D.println(D.VERBOSE, "device ready");
             this.reply.set(0, 0, 0, 0);
@@ -174,13 +170,13 @@ public class SCSIcdimage extends SCSI {
         this.out.flush();
     }
 
-    void client_read_capacity() throws IOException {
+    private void client_read_capacity() throws IOException {
         byte[] arrayOfByte = {0, 0, 0, 0, 0, 0, 0, 0};
 
         this.reply.set(0, 0, 0, arrayOfByte.length);
-        if (this.fdd_state == 0) {
+        if (this.fddState == 0) {
             this.reply.set(2, 58, 0, 0);
-        } else if (this.fdd_state == 1) {
+        } else if (this.fddState == 1) {
             this.reply.set(6, 40, 0, 0);
         } else {
             int i = (int) (this.media.size() / 2048L - 1L);
@@ -191,14 +187,14 @@ public class SCSIcdimage extends SCSI {
             arrayOfByte[6] = 8;
         }
         this.reply.send(this.out);
-        if (this.fdd_state == 2)
+        if (this.fddState == 2)
             this.out.write(arrayOfByte, 0, arrayOfByte.length);
         this.out.flush();
         D.println(D.VERBOSE, "client_read_capacity: ");
         D.hexdump(D.VERBOSE, arrayOfByte, 8);
     }
 
-    void client_read_toc(byte[] request) throws IOException {
+    private void client_read_toc(byte[] request) throws IOException {
         int i = (request[1] & 0x2) != 0 ? 1 : 0;
         int j = (request[9] & 0xC0) >> 6;
         int k = (int) (this.media.size() / 2048L);
@@ -262,7 +258,7 @@ public class SCSIcdimage extends SCSI {
         this.out.flush();
     }
 
-    void client_mode_sense(byte[] request) throws IOException {
+    private void client_mode_sense(byte[] request) throws IOException {
         this.buffer[0] = 0;
         this.buffer[1] = 8;
         this.buffer[2] = 1;
@@ -279,7 +275,7 @@ public class SCSIcdimage extends SCSI {
         this.out.flush();
     }
 
-    void client_get_event_status(byte[] request) throws IOException {
+    private void client_get_event_status(byte[] request) throws IOException {
         int i = request[4];
         int j = SCSI.mk_int16(request, 7);
         for (int k = 0; k < j; k++) {
@@ -297,21 +293,21 @@ public class SCSIcdimage extends SCSI {
             this.buffer[1] = 6;
             this.buffer[2] = 4;
             this.buffer[3] = 16;
-            if (this.event_state == 0) {
+            if (this.eventState == 0) {
                 this.buffer[4] = 0;
                 this.buffer[5] = 0;
-            } else if (this.event_state == 1) {
+            } else if (this.eventState == 1) {
                 this.buffer[4] = 4;
                 this.buffer[5] = 2;
 
                 if (j > 4)
-                    this.event_state = 2;
-            } else if (this.event_state == 4) {
+                    this.eventState = 2;
+            } else if (this.eventState == 4) {
                 this.buffer[4] = 3;
                 this.buffer[5] = 0;
 
                 if (j > 4) {
-                    this.event_state = 0;
+                    this.eventState = 0;
                 }
             } else {
                 this.buffer[4] = 0;
